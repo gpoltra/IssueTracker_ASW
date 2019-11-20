c6ee0e8118c0d9386bf60d9b28d45bb1c695ce75
paubb <pau.ballber@est.fib.upc.edu>
2019-11-20 13:04:00 +0000

    look and feel del show

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,18 @@
 class Micropost < ApplicationRecord
-    belongs_to :user
-    belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id, optional: true
-    has_many :comments, dependent: :destroy
-    has_many :votes, dependent: :destroy
-    has_many :watchers, dependent: :destroy
+  belongs_to :assignee, class_name: 'User', foreign_key: :assignee_id, optional: true
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
+  has_many :watchers, dependent: :destroy
   validates :description, length: { maximum: 240 }
-  #validates :user_id, presence: true
+  
+  def self.status
+    ["New", "Open", "On hold", "Resolved", "Duplicate", "Invalid", "Won't fix", "Closed"]
+  end
+  def self.type
+      ["Bug", "Enhancement", "Proposal", "Task"]
+  end
+  def self.priority
+      ["Trivial", "Minor", "Major", "Critical", "Blocker"]
+  end
+    #validates :user_id, presence: true
 end
