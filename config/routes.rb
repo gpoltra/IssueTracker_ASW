39d11b814792fa10ee0df2657e8e29682d555af0
RoxasT <atro.roser@gmail.com>
2019-11-20 17:57:20 +0000

    Login Google pero dona error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  #get ‘login’, to: ‘sessions#new’, as: ‘login’
   resources :microposts
   resources :users
+  resources :sessions
   resources :microposts do
     resources :comments
   end
+  get "/auth/google_oauth2/callback", to: "sessions#create"
+  get "/login", to: redirect("/auth/google_oauth2")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#new'
 end
