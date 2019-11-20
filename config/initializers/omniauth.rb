735859ae2ad98fd7a8abf4b17008e006fc726f34
paubb <pau.ballber@est.fib.upc.edu>
2019-11-20 19:00:18 +0000

    arreglat votes

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV[1090313193941-eatabovkihgiamrvvmb3neaidt3nhdu2.apps.googleusercontent.com], ENV[dPiLN51oJEOMOZg-EHh5OmQ7]
-end
