771979b1020335f104e2e0a5e402d174cf6c10fc
gpoltra98 <gpoltra98@gmail.com>
2019-11-28 11:17:49 +0000

    some bugs

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -32,7 +32,7 @@ class MicropostsController < ApplicationController
     
     if params.has_key?(:watcher)
         if User.exists?(id: params[:watcher])
-          @microposts = Micropost.joins(:watchers).where(watchers:{user_id: params[:watcher]})
+          @microposts = Micropost.includes(:Watchers).where(watchers:{user_id: params[:watcher]})
         else
           format.json {render json: {"error":"User with id="+params[:watcher]+" does not exist"}, status: :unprocessable_entity}
         end
@@ -110,7 +110,7 @@ class MicropostsController < ApplicationController
         @vote.destroy
         @issue_to_vote.decrement!(:votes)
       end
-      format.html { redirect_to @issue_to_vote }
+      format.html {redirect_to request.referrer}
       format.json { render json: @issue_to_vote, status: :ok }
     end
   end
@@ -129,11 +129,7 @@ class MicropostsController < ApplicationController
         @watcher.destroy
         @issue_to_watch.decrement!("watchers")
       end
-      if params[:view] == "index"
-        format.html { redirect_to issues_url}
-      else
-        format.html { redirect_to @issue_to_watch}
-      end
+      format.html {redirect_to request.referrer}
       format.json { render json: @issue_to_watch, status: :ok }
     end
   end
