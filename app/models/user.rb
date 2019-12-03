ee7a98f1d7b29c9882dec3158420249ef6b4ea16
paubb <pau.ballber@est.fib.upc.edu>
2019-12-03 19:25:49 +0000

    error eliminar usuaris que estan a la taula watchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :watchers, dependent: :destroy
 
   attr_accessor :password, :password_confirmation
   validates :name, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
