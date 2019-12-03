6f149d9a0c40c7c74d813e354e3023c65a2b2d42
paubb <pau.ballber@est.fib.upc.edu>
2019-12-03 17:17:46 +0000

    prova get current user tret

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,16 +73,6 @@ class UsersController < ApplicationController
     end
   end
   
-  def obtain_current_user
-    respond_to do |format|
-      if (!current_user.nil?) 
-        @user = current_user
-        format.html
-        format.json {render json: @user, status: :ok}
-      end
-    end
-  end
-  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
