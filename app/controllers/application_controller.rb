be9f244132baee9d0657e1de78b4d6777a3f3a67
paubb <pau.ballber@est.fib.upc.edu>
2019-12-03 16:02:24 +0000

    protect_form_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-    def hello
-    render html: "hello, world!"
-    end
+    protect_from_forgery with: :null_session
     include SessionsHelper
+    
 end
