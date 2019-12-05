cba4f56e65a5c04c5e49d8318b85114cd407a94b
paubb <pau.ballber@est.fib.upc.edu>
2019-12-05 13:23:07 +0000

    prova comments2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -95,6 +95,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.permit(:body)
+      params.permit(:body, :user_id)
     end
 end
